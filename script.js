e0619b18e6849d557a7a14956640cd08fd16a750
michael <michaeladams946@gmail.com>
2023-06-04 18:12:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var quotes = [
-  "The only way to do great work is to love what you do. - Steve Jobs",
-  "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-  "In a gentle way, you can shake the world. - Mahatma Gandhi",
-  "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer"
-];
-
-function changeText() {
-  var randomIndex = Math.floor(Math.random() * quotes.length);
-  document.getElementById("demo").innerHTML = quotes[randomIndex];
-}
+function changeTextAndColor() {
+    var colors = ['#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107'];
+    var quotes = [
+      'The only way to do great work is to love what you do.',
+      'Believe you can and you’re halfway there.',
+      'Don’t wait. The time will never be just right.',
+      'Success is not the key to happiness. Happiness is the key to success.',
+      'Success usually comes to those who are too busy to be looking for it.'
+    ];
+    
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  
+    document.body.style.backgroundColor = randomColor;
+    document.getElementById('demo').innerText = randomQuote;
+  }
